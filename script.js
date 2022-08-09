f9fe2d8ff2718997b9ac231195b80a9164109ffd
Diogo Martins <me.martinsfe@gmail.com>
2022-08-09 00:18:32 -0300

    feat: complete item 3 and 4

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Acessando elementos
 const paraColorToGuess = document.querySelector('#rgb-color');
+const colorCirclesOptions = document.querySelector('.guess-color');
 
 // Criando elementos
-
+const createList = document.createElement('LI');
 
 const randColor = () => {
   let red = Math.floor(Math.random() * 256);
@@ -13,7 +14,29 @@ const randColor = () => {
 }
 
 
-console.log(paraColorToGuess)
-console.log(randColor())
 
-paraColorToGuess.innerText = randColor();
+const playerChoice = (choice) => {
+  console.log(choice.target)
+}
+
+const genColorOptions = (numOptions) => {
+  for (let i = 0; i < numOptions; i += 1) {
+    const createList = document.createElement('LI');
+    createList.classList.add('ball');
+    createList.style.backgroundColor = randColor();
+    createList.addEventListener('click', playerChoice)
+    // createList.innerText = i + 1;
+    colorCirclesOptions.append(createList);
+  }
+};
+
+
+genColorOptions(6);
+
+const listColorToGuess = document.querySelectorAll('LI');
+const colorToGuess = Math.floor(Math.random() * listColorToGuess.length);
+
+console.log(colorToGuess)
+console.log(listColorToGuess)
+
+paraColorToGuess.innerText = listColorToGuess[colorToGuess].style.backgroundColor;
