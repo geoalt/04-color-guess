5f4720b6a9eb7441036ed550c407abfffe319f82
Diogo Martins <me.martinsfe@gmail.com>
2022-08-09 02:47:28 -0300

    feat: complete item 5

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Acessando elementos
 const paraColorToGuess = document.querySelector('#rgb-color');
 const colorCirclesOptions = document.querySelector('.guess-color');
+const paraAnswer = document.querySelector('#answer');
+
+
 
 // Criando elementos
 const createList = document.createElement('LI');
@@ -15,28 +18,33 @@ const randColor = () => {
 
 
 
-const playerChoice = (choice) => {
-  console.log(choice.target)
-}
-
 const genColorOptions = (numOptions) => {
   for (let i = 0; i < numOptions; i += 1) {
     const createList = document.createElement('LI');
     createList.classList.add('ball');
     createList.style.backgroundColor = randColor();
-    createList.addEventListener('click', playerChoice)
-    // createList.innerText = i + 1;
+    createList.addEventListener('click', playerChoice);
     colorCirclesOptions.append(createList);
   }
 };
+let colorToGuess;
+const genGuessTheColor = () => {
+  const listColorToGuess = document.querySelectorAll('LI');
+  const pickRandomColor = Math.floor(Math.random() * listColorToGuess.length);
+  colorToGuess = listColorToGuess[pickRandomColor].style.backgroundColor;
+  paraColorToGuess.innerText = colorToGuess;
+};
 
+const playerChoice = (choice) => {
+  if (choice.target.style.backgroundColor === colorToGuess) {
+    paraAnswer.innerText = 'Acertou!';
+  } else {
+    paraAnswer.innerText = 'Errou! Tente novamente!';
+  }
+};
 
-genColorOptions(6);
+window.onload = () => {
+  genColorOptions(6);
+  genGuessTheColor();
+}
 
-const listColorToGuess = document.querySelectorAll('LI');
-const colorToGuess = Math.floor(Math.random() * listColorToGuess.length);
-
-console.log(colorToGuess)
-console.log(listColorToGuess)
-
-paraColorToGuess.innerText = listColorToGuess[colorToGuess].style.backgroundColor;
